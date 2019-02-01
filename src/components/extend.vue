aa4eeae53b07494598915b26fe68c7a9745e4971
shenpengfei <shenpengfei@didiglobal.com>
2019-02-01 13:57:49 +0800

    增加extend

---
+++ src/components/extend.vue
@@ -0,0 +1,28 @@
+<!-- this is test extend and mixins  -->
+<template>
+  <div class='extends-wrapper'>
+    <h1>this is test extend </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+
+  components: {},
+
+  computed: {},
+
+  filters: {},
+
+  methods: {},
+
+  mounted () {}
+
+}
+</script>
+<style lang='scss' scoped>
+</style>
