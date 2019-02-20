a45bfd48209dec2a3d0b2211a02f47c2c6433c0a
shenpengfei <shenpengfei@didiglobal.com>
2019-02-20 22:41:53 +0800

    aa

--- src/util/common.js
+++ src/util/common.js
@@ -1,7 +1,15 @@
 export default class Time extends Date{
 
-  format(){
-      return `${this.getFullYear()}-${(this.getMonth() +1 ).toString().padStart(2,0)}-${(this.getDate()).toString().padStart(2,0)} ${(this.getHours()).toString().padStart(2,0)}:${(this.getMinutes()).toString().padStart(2,0)}:${(this.getSeconds()).toString().padStart(2,0)}`
+  format(type = 'YYYY-MM-DD HH:mm:ss'){
+    let year = this.getFullYear()
+    let month = this.getMonth().toString().padStart(2,0)
+    let day = this.getDate().toString().padStart(2,0)
+    let hours = this.getHours().toString().padStart(2,0)
+    let minute = this.getMinutes().toString().padStart(2,0)
+    let second = this.getSeconds().toString().padStart(2,0)
+
+    return type.replace(/YYYY([^\d])MM([^\d])DD([^\d])HH([^\d])mm([^\d])ss/,`${year}$1${month}$2${day}$3${hours}$4${minute}$5${second}`)
   }
 
 }
+
